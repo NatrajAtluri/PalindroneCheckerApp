af7c4e5c25e53936f824ff418fb9c481050e47cd
Natraj Atluri <natrajatluri@gmail.com>
2026-03-06 09:09:54 +0530

    [UC6] Added USECASE 6

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,5 +1,7 @@
-import java.util.Scanner;
+import java.util.*;
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class PalindroneCheckerApp {
 
@@ -7,23 +9,30 @@ public class PalindroneCheckerApp {
 
         Scanner scanner = new Scanner(System.in);
         Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
 
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
 
-        // Push characters into stack
+        // Insert characters into stack and queue
         for (int i = 0; i < input.length(); i++) {
-            stack.push(input.charAt(i));
+            char ch = input.charAt(i);
+            stack.push(ch);      // LIFO
+            queue.add(ch);       // FIFO
         }
 
-        // Pop characters to create reversed string
-        String reversed = "";
+        boolean isPalindrome = true;
+
+        // Compare dequeue and pop
         while (!stack.isEmpty()) {
-            reversed += stack.pop();
+            if (stack.pop() != queue.remove()) {
+                isPalindrome = false;
+                break;
+            }
         }
 
-        // Compare original and reversed strings
-        if (input.equals(reversed)) {
+        // Print result
+        if (isPalindrome) {
             System.out.println("Result: The string is a Palindrome.");
         } else {
             System.out.println("Result: The string is NOT a Palindrome.");
